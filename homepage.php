5811c5c2d7839a2315413899c458060a0afffbd6
exam295 <exam295@truro-penwith.ac.uk>
2022-03-07 14:11:26 +0000

    Start of all the webpages
    all webpages created and getting started on the content of the homepage

---
+++ homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>GibJohn Tutoring Homepage</title>
+  <meta charset="utf-8">
+  <!-- These are bootstrap links -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="homepage.php">GibJohn Tutoring</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="homepage.php">Home</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="login.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="register.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+</body>
+</html>
